bf05f9f33e2785ae23edb422a1825c303ecae919
中野 瑛介 <eisuke.nakano@nsw.co.jp>
2026-02-20 13:18:29 +0900

    列挙作成

---
+++ src/enums/months.ts
@@ -0,0 +1,21 @@
+import type { SelectOption } from "../components/Select";
+
+export const MONTHS: SelectOption[] = [
+  { value: "1", label: "1月" },
+  { value: "2", label: "2月" },
+  { value: "3", label: "3月" },
+  { value: "4", label: "4月" },
+  { value: "5", label: "5月" },
+  { value: "6", label: "6月" },
+  { value: "7", label: "7月" },
+  { value: "8", label: "8月" },
+  { value: "9", label: "9月" },
+  { value: "10", label: "10月" },
+  { value: "11", label: "11月" },
+  { value: "12", label: "12月" },
+];
+
+export const HALF: SelectOption[] = [
+  { value: "1", label: "上半期(4月〜6月)" },
+  { value: "2", label: "下半期(10月〜3月)" },
+];
