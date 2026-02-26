91580441cff419a8f2916b3dc555645d55945056
中野 瑛介 <eisuke.nakano@nsw.co.jp>
2026-02-26 17:18:09 +0900

    prismaセットアップ

---
+++ prisma/migrations/20260226081415_first/migration.sql
@@ -0,0 +1,9 @@
+-- CreateTable
+CREATE TABLE "Test" (
+    "id" SERIAL NOT NULL,
+    "memo" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Test_pkey" PRIMARY KEY ("id")
+);
